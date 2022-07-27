14a448b1157d06c71ccc2bec153be2ae13c5864a
Obafunso Ridwan Adebayo <0xridwanobafunso@gmail.com>
2022-07-27 11:25:05 +0100

    designed QCardContainer to house other components

---
+++ src/components/QCardContainer.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="card-container"></div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "QCardContainer",
+  components: {},
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style scoped>
+.card-container {
+  background-color: #fff;
+  margin-top: 20px;
+  padding: 35px;
+  border-width: 1px;
+  border-radius: 1.5rem;
+}
+</style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -2,6 +2,7 @@
   <q-page>
     <div class="main-section">
       <q-breadcrumb></q-breadcrumb>
+      <q-card-container></q-card-container>
     </div>
   </q-page>
 </template>
@@ -9,11 +10,13 @@
 <script>
 import { defineComponent } from "vue";
 import QBreadcrumb from "../components/QBreadcrumb.vue";
+import QCardContainer from "../components/QCardContainer.vue";
 
 export default defineComponent({
   name: "IndexPage",
   components: {
     QBreadcrumb,
+    QCardContainer,
   },
 
   setup() {},
