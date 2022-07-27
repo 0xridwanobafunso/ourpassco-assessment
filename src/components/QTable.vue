63049a4348ea15159e7f6606c98c7d19a21dff0f
Obafunso Ridwan Adebayo <0xridwanobafunso@gmail.com>
2022-07-27 10:42:40 +0100

    bootstrap quasar framework, installed miragejs, designed navigation and breadcrumb component

---
+++ src/components/QTable.vue
@@ -0,0 +1,15 @@
+<template></template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "QTable",
+  components: {},
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style scoped></style>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,27 @@
+<template>
+  <q-page>
+    <div class="main-section">
+      <q-breadcrumb></q-breadcrumb>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import QBreadcrumb from "../components/QBreadcrumb.vue";
+
+export default defineComponent({
+  name: "IndexPage",
+  components: {
+    QBreadcrumb,
+  },
+
+  setup() {},
+});
+</script>
+
+<style scoped>
+.main-section {
+  padding: 30px 25px;
+}
+</style>
